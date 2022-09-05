79046b561826fb2b94be57760c01bbf5dab8befd
Jordan <jordan.tibi@inseec-france.com>
2022-09-05 12:34:41 +0200

    Controls

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,35 +7,74 @@ using static UnityEngine.InputSystem.InputSettings;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] InputActionReference _moveInput;
-
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] float _speed;
-    [SerializeField] float _movingTreshold;
+    [SerializeField] InputActionReference _move;
+    [SerializeField] InputActionReference _sprint;
+    [SerializeField] Rigidbody2D _rb;
     [SerializeField] Animator _animator;
+
+    [SerializeField] float _speed;
+
+    Vector3 _direction;
+    Vector3 _aimDirection;
+    bool _isRunning;
 
     private void Start()
     {
-        _moveInput.action.started += StartMove; ;
-        _moveInput.action.performed += UpdateMove; ;
-        _moveInput.action.canceled += EndMove; ;
+        _move.action.started += StartMove;
+        _move.action.performed += UpdateMove;
+        _move.action.canceled += StopMove;
 
-        
+        //_sprint.action.started += StartSprint;
+        //_sprint.action.canceled += StopSprint;
+    }
+
+    private void StopSprint(InputAction.CallbackContext obj)
+    {
+        _isRunning = false;
+    }
+
+    private void StartSprint(InputAction.CallbackContext obj)
+    {
+        _isRunning = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log($"{_direction}");
+
+        _animator.SetFloat("Horizontal", _aimDirection.x);
+        _animator.SetFloat("Vertical", _aimDirection.y);
+        _animator.SetBool("IsMoving", _direction.magnitude > 0.1f);
+        _animator.SetBool("IsRunning", _isRunning);
+
+        _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed));
+
+
+        //if (_isRunning)
+        //{
+        //    _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed * 2));
+        //}
+        //else
+        //{
+        //    _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed));
+        //}
 
     }
 
-    private void EndMove(InputAction.CallbackContext obj)
+    private void StopMove(InputAction.CallbackContext obj)
     {
-        throw new System.NotImplementedException();
+        _direction = Vector3.zero;
     }
 
     private void UpdateMove(InputAction.CallbackContext obj)
     {
-        throw new System.NotImplementedException();
+        _direction = obj.ReadValue<Vector2>();
+        _aimDirection = _direction;
     }
 
     private void StartMove(InputAction.CallbackContext obj)
     {
-        throw new System.NotImplementedException();
+        _direction = obj.ReadValue<Vector2>();
+        _aimDirection = _direction;
     }
 }
